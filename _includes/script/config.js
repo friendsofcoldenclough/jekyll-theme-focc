6adf474b470d7960269b1e601ba132d3d5ab5ee9
bjorsq <pete@bjorsq.net>
2026-02-15 16:58:08 +0000

    Changed the way styles and scripts are cimpiled in theme

---
+++ _includes/script/config.js
@@ -0,0 +1,7 @@
+var focc = {
+    currentLoc: {
+        lat: 53.74691335748559,
+        lng: -2.0338482556515936
+    },
+    startZoom: 15
+};
--- _scripts/minify.js
+++ _scripts/minify.js
@@ -5,41 +5,40 @@ const fs = require('fs');
 const path = require('path');
 const UglifyJS = require("uglify-js");
 const sass = require('sass');
-const { exec } = require('child_process');
+const getSRI = require('get-sri');
 
-const compiled_js = path.resolve( __dirname, '../assets/js/script.min.js' );
-const compiled_css = path.resolve( __dirname, '../assets/css/style.min.css' );
+const compiled_js = path.resolve( __dirname, '../assets/js/script.js' );
+const minified_js = path.resolve( __dirname, '../assets/js/script.min.js' );
+const compiled_css = path.resolve( __dirname, '../assets/css/style.css' );
+const minified_css = path.resolve( __dirname, '../assets/css/style.min.css' );
 const integrity_file = path.resolve( __dirname, '../_data/integrity.json' );
-
 const jsdir = '../_includes/script/';
 
-var result = sass.compile( path.resolve( __dirname, '../_scss/style.scss' ), {style: "compressed"} );
+var result = sass.compile( path.resolve( __dirname, '../_scss/style.scss' ) );
 fs.writeFileSync( compiled_css, result.css );
+result = sass.compile( path.resolve( __dirname, '../_scss/style.scss' ), {style: "compressed"} );
+fs.writeFileSync( minified_css, result.css );
 
-fs.writeFileSync( compiled_js, UglifyJS.minify({
-    "utilities.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'utilities.js' ), "utf8" ),
-    "leaflet.fullscreen.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'leaflet.fullscreen.js' ), "utf8" ),
-    "maps.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'maps.js' ), "utf8" )
-}, { toplevel: true }).code, "utf8" );
-
+fs.writeFileSync( compiled_js, '');
+for ( const file of [ 'config.js', 'utilities.js' ] ) {
+    fs.appendFileSync( compiled_js, fs.readFileSync( path.resolve( __dirname, jsdir, file ), "utf8" ) );
+}
+fs.writeFileSync( minified_js, UglifyJS.minify({
+    "config.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'config.js' ), "utf8" ),
+    "utilities.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'utilities.js' ), "utf8" )
+}, { toplevel: false }).code, "utf8" );
 var integrity = {
     "script": "",
-    "style": ""
+    "style": "",
+    "script.min": "",
+    "style.min": ""
 };
-exec('openssl dgst -sha384 -binary ' + compiled_js + ' | openssl base64 -A', (err, stdout, stderr) => {
-    if (err) {
-        console.log( "Couldn't execute the command: " + stderr );
-        return;
+for( const key in integrity ) {
+
+    if ( fs.existsSync( path.resolve( __dirname, '../assets/js/', key + '.js' ) ) ) {
+        integrity[key] = getSRI(fs.readFileSync( path.resolve( __dirname, '../assets/js/', key + '.js' ), 'utf8' ), getSRI.SHA384, true );
+    } else if ( fs.existsSync( path.resolve( __dirname, '../assets/css/', key + '.css' ) ) ) {
+        integrity[key] = getSRI(fs.readFileSync( path.resolve( __dirname, '../assets/css/', key + '.css' ), 'utf8' ), getSRI.SHA384, true );
     }
-    console.log( "Updating integrity.json with script hash: " + stdout );
-    integrity.script = stdout;
-    exec('openssl dgst -sha384 -binary ' + compiled_css + ' | openssl base64 -A', (err, stdout, stderr) => {
-        if (err) {
-            console.log( "Couldn't execute the command: " + stderr );
-            return;
-        }
-        console.log( "Updating integrity.json with style hash: " + stdout );
-        integrity.style = stdout;
-        fs.writeFileSync( integrity_file, JSON.stringify( integrity ), 'utf8' );
-    });
-});
+}
+fs.writeFileSync( integrity_file, JSON.stringify( integrity ), 'utf8' );
